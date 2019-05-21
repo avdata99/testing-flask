87f1f61e595541f3c8dda59f3a402d940ea77b3a
avdata99 <andres@data99.com.ar>
2019-05-20 23:50:06 -0300

    blog andando

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,12 +24,14 @@ def create_app(test_config=None):
     except OSError:
         pass
     
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     from . import db
     db.init_app(app)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     
     return app
